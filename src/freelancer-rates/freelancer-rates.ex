7ada04282dae1d173fdc8638d7abe61477e61d52
Adrian Garrido <agarrido@qdqmedia.com>
2021-11-23 17:53:34 +0100

    Use module attributes and pipe operators

--- src/freelancer-rates/freelancer-rates.ex
+++ src/freelancer-rates/freelancer-rates.ex
@@ -1,20 +1,20 @@
 defmodule FreelancerRates do
+  @hourly_rate_on_daily_rate 8.0
+  @monthly_billable_days 22
+
   def daily_rate(hourly_rate) do
-    number_of_hourly_rates_on_daily_rate = 8.0
-    hourly_rate * number_of_hourly_rates_on_daily_rate
+    hourly_rate * @hourly_rate_on_daily_rate
   end
 
   def apply_discount(before_discount, discount) do
-    before_discount * ((100.0 - discount) / 100)
+    before_discount * (100.0 - discount) / 100
   end
 
   def monthly_rate(hourly_rate, discount) do
-    number_of_daily_rate_on_monthly_rate = 22
-    cond do
-      discount == 100.0 or discount == 100 -> 0.0
-      discount == 0.0 or discount == 0 -> trunc(Float.ceil(daily_rate(hourly_rate) * number_of_daily_rate_on_monthly_rate))
-      true -> trunc(Float.ceil(apply_discount(daily_rate(hourly_rate), discount) * number_of_daily_rate_on_monthly_rate))
-    end
+    daily_rate(hourly_rate)
+    |> apply_discount(discount)
+    |> Kernel.*(@monthly_billable_days)
+    |> ceil
   end
 
   def days_in_budget(budget, hourly_rate, discount) do
